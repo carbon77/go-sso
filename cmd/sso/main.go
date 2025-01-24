8d7a7b6244700f9a7ef3655a7387d3a86a4ab9e5
Igor <zakatov0303@gmail.com>
2025-01-24 21:55:03 +0300

    Add logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/carbon77/sso/internal/config"
 )
 
+const (
+	envDev  = "dev"
+	envProd = "prod"
+)
+
 func main() {
 	config := config.MustLoad()
-	fmt.Println(config)
 
-	// TODO: init logger
+	log := setupLogger(config.Env)
+	log.Info(
+		"starting application",
+		slog.String("env", config.Env),
+	)
 
 	// TODO: init app
 
 	// TODO: start grpc-server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+
+	return log
 }
